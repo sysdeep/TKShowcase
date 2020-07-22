8b0c302099ce941d2b032bf239c18fd9417002e3
Diver Dune <sysdeep@yandex.ru>
2020-07-22 11:45:45 +0300

    tcl - widgets pane ready

--- TCL/demo_1/widget_pane.tcl
+++ TCL/demo_1/widget_pane.tcl
@@ -2,7 +2,7 @@
 frame .n.widgetPane
 
 
-
+#--- text entries -------------------------------------------------------------
 pack [labelframe .n.widgetPane.lf -text "Text Entry"] -side top -fill both -padx 10 -pady 10
 # button .widgetPane.button -text "wp"
 # grid .widgetPane.button
@@ -15,7 +15,7 @@ pack .n.widgetPane.lf.text -side bottom -fill both -anchor nw -padx 5
 # -width 0 -height 3 -text "import std.stdio;\n\nvoid main(string[] args)\n{\n\twriteln(\"Hello World!\");\n}"
 # button .n.widgetPane.exit_button -text Exit -command {exit} -width 10
 # 
-pack [entry .n.widgetPane.lf.entry ] -side left -fill x -anchor nw -padx 5
+pack [entry .n.widgetPane.lf.entry ] -side left -fill x -anchor nw -padx 5 -expand true
 # this._entry = new Entry(entryLabelFrame)
 			# 	.pack(5, 0, GeometrySide.left, GeometryFill.x, AnchorPosition.northWest, true);
 
@@ -31,81 +31,68 @@ pack .n.widgetPane.lf.spinbox -side left -padx 5
 
 
 
-pack [ttk::combobox .n.widgetPane.lf.combobox ] -side left -fill x -anchor nw -padx 5
-			# auto entry4 = new ComboBox(entryLabelFrame)
-			# 	.setData(["Option 1", "Option 2", "Option 3"])
-			# 	.setValue("Option 1")
-			# 	.pack(5, 0, GeometrySide.left, GeometryFill.x, AnchorPosition.northWest, true);
+pack [ttk::combobox .n.widgetPane.lf.combobox -values "Option_1 Option_2 Option_3"] -side left -fill x -anchor nw -padx 5
+.n.widgetPane.lf.combobox current 0
+			
+#--- text entries -------------------------------------------------------------
 
 
 
-#--- rangeLabelFrame
+
+
+#--- rangeLabelFrame ----------------------------------------------------------
 pack [labelframe .n.widgetPane.rangeLabelFrame -text "Progress & Scale"] \
 	-side bottom -fill both -anchor center -padx 10 -pady 10
 
+#--- progress
 pack [ttk::progressbar .n.widgetPane.rangeLabelFrame.progress\
 	-value 4 -maximum 10
 ] -padx 5 -pady 5 -side top -fill x -anchor center
 
-pack [ttk::scale .n.widgetPane.rangeLabelFrame.scale -from 10 -to 0 -value 4] \
-	-padx 5 -pady 5 -side top -fill x -anchor center
-
-# auto rangeLabelFrame = new LabelFrame(widgetPane, "Progress & Scale")
-# 			.pack(10, 0, GeometrySide.bottom, GeometryFill.both, AnchorPosition.center);
-# 			this._progressBar = new ProgressBar(rangeLabelFrame)
-# 				.setMaxValue(10)
-# 				.setValue(4)
-# 				.pack(5, 0, GeometrySide.top, GeometryFill.x, AnchorPosition.center, true);
-# 			auto scale = new Scale(rangeLabelFrame)
-# 				.setFromValue(10)
-# 				.setToValue(0)
-# 				.setCommand(delegate(CommandArgs args){
-# 					auto scale = cast(Scale)args.element;
-# 					this._progressBar.setValue(scale.getValue());
-# 				})
-# 				.setValue(4)
-# 				.pack(5, 0, GeometrySide.top, GeometryFill.x, AnchorPosition.center, true);
+#--- scale
+proc do_scale {w_progress value} {
+	$w_progress configure -value $value
+}
+ttk::scale .n.widgetPane.rangeLabelFrame.scale -from 10 -to 0 -value 4 -command "do_scale .n.widgetPane.rangeLabelFrame.progress"
+pack .n.widgetPane.rangeLabelFrame.scale -padx 5 -pady 5 -side top -fill x -anchor center
+#--- rangeLabelFrame ----------------------------------------------------------
 
 
 
+#--- Buttons ------------------------------------------------------------------
+labelframe .n.widgetPane.buttonsFrame -text "Buttons"
+pack .n.widgetPane.buttonsFrame -padx 10 -pady 10 -side left -fill both -expand true -anchor center
+
+pack [button .n.widgetPane.buttonsFrame.b1 -text "Text button"] -padx 5 -pady 5
+
+pack [button .n.widgetPane.buttonsFrame.b2 -text "Image button" -image [image create photo -file "./media/thumbnail.png"] -compound left ] -padx 5 -pady 5
+
+menubutton .n.widgetPane.buttonsFrame.b3 -text "Menu button" -menu .n.widgetPane.buttonsFrame.b3.menu
+pack .n.widgetPane.buttonsFrame.b3 -padx 5 -pady 5
+
+menu .n.widgetPane.buttonsFrame.b3.menu -tearoff false
+.n.widgetPane.buttonsFrame.b3.menu add command -label "Option 1"
+.n.widgetPane.buttonsFrame.b3.menu add command -label "Option 2"
+.n.widgetPane.buttonsFrame.b3.menu add command -label "Option 3"
 
 
-# 		auto buttonLabelFrame = new LabelFrame(widgetPane, "Buttons")
-# 			.pack(10, 0, GeometrySide.left, GeometryFill.both, AnchorPosition.center, true);
-# 			auto button1 = new Button(buttonLabelFrame, "Text button")
-# 				.pack(5);
-# 			auto button2 = new Button(buttonLabelFrame, new EmbeddedPng!("thumbnail.png"), "Image button", ImagePosition.left)
-# 				.pack(5);
-# 			auto buttonMenu = new Menu()
-# 				.addEntry("Option 1", delegate(CommandArgs args){})
-# 				.addEntry("Option 2", delegate(CommandArgs args){})
-# 				.addEntry("Option 3", delegate(CommandArgs args){});
-# 			auto button3 = new MenuButton(buttonLabelFrame, "Menu button", buttonMenu)
-# 				.pack(5);
 
-# 		auto checkBoxLabelFrame = new LabelFrame(widgetPane, "Check buttons")
-# 			.pack(10, 0, GeometrySide.left, GeometryFill.both, AnchorPosition.center, true);
-# 			auto checkButton1 = new CheckButton(checkBoxLabelFrame, "Option 1")
-# 				.check()
-# 				.pack(5);
-# 			auto checkButton2 = new CheckButton(checkBoxLabelFrame, "Option 2")
-# 				.pack(5);
-# 			auto checkButton3 = new CheckButton(checkBoxLabelFrame, "Option 3")
-# 				.pack(5);
+labelframe .n.widgetPane.checkButtonsFrame -text "Check buttons"
+pack .n.widgetPane.checkButtonsFrame -padx 10 -pady 10 -side left -fill both -expand true -anchor center
 
-# 		auto radioLabelFrame = new LabelFrame(widgetPane, "Radio buttons")
-# 			.pack(10, 0, GeometrySide.left, GeometryFill.both, AnchorPosition.center, true);
-# 			auto radioButton1 = new RadioButton(radioLabelFrame, "Option 1")
-# 				.setSelectedValue("1")
-# 				.select()
-# 				.pack(5);
-# 			auto radioButton2 = new RadioButton(radioLabelFrame, "Option 2")
-# 				.setSelectedValue("2")
-# 				.pack(5);
-# 			auto radioButton3 = new RadioButton(radioLabelFrame, "Option 3")
-# 				.setSelectedValue("3")
-# 				.pack(5);
+pack [checkbutton .n.widgetPane.checkButtonsFrame.b1 -text "Option 1"] -padx 5 -pady 5
+pack [checkbutton .n.widgetPane.checkButtonsFrame.b2 -text "Option 2"] -padx 5 -pady 5
+pack [checkbutton .n.widgetPane.checkButtonsFrame.b3 -text "Option 3"] -padx 5 -pady 5
+.n.widgetPane.checkButtonsFrame.b1 select
 
 
+labelframe .n.widgetPane.radioButtonsFrame -text "Radio buttons"
+pack .n.widgetPane.radioButtonsFrame -padx 10 -pady 10 -side left -fill both -expand true -anchor center
+
+pack [radiobutton .n.widgetPane.radioButtonsFrame.b1 -text "Option 1" -value 1] -padx 5 -pady 5
+pack [radiobutton .n.widgetPane.radioButtonsFrame.b2 -text "Option 2" -value 2] -padx 5 -pady 5
+pack [radiobutton .n.widgetPane.radioButtonsFrame.b3 -text "Option 3" -value 3] -padx 5 -pady 5
+.n.widgetPane.radioButtonsFrame.b1 select
+#--- Buttons ------------------------------------------------------------------
 
 # pack .n.widgetPane.exit_button -padx 5 -pady 5
