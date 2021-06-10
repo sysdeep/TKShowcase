96c862d3d6cc1bb345193db7da5ed98b8062ab24
Diver Dune <sysdeep@yandex.ru>
2021-06-10 09:09:49 +0300

    fix tcl demo menu theme

--- TCL/demo_1/frame_menu.tcl
+++ TCL/demo_1/frame_menu.tcl
@@ -35,48 +35,6 @@ menu .main_menu.file.radio -tearoff 0
 .main_menu.file add command -label Exit -image img_close -compound left -accelerator "Ctrl-q" -command { exit }
 
 
-# proc exx {theme_name} \
-# {
-# 	puts $theme_name
-# 	ttk::style theme use $theme_name
-# }
-
-# proc lambda {args body} {
-# 	puts $args
-# 	puts $body
-# 	set name {}
-#     proc $name $args $body
-
-#     puts $name
-#     return $name
-
-# 	# puts $args
-# 	# # proc $body $args
-# 	# # list ::apply [list $args [list expr $body]]
-#  #        set name [info level 0]
-#  #        proc $name $args $body
-#  #        set name
-#  }
-
-
-# proc lambda {params body} {
-#     list apply [list $params $body]
-# }
-
-# proc lambda1 {name} {
-	
-
-# 	proc exx {} \
-# 	{
-# 		ttk::style theme use $name
-# 	}
-
-# 	return exx
-
-# }
-
-
-
 
 #--- themes menu
 menu .main_menu.themes -tearoff 0
@@ -84,11 +42,7 @@ menu .main_menu.themes -tearoff 0
 
 #--- проблемы с lambda... непонятно как использовать...
 foreach theme_name [ttk::style theme names] {
-	# .main_menu.themes add command -label $theme_name -command { ttk::style theme use $theme_name }
-	# .main_menu.themes add command -label $theme_name -command { exx $theme_name }
-	# .main_menu.themes add command -label $theme_name -command [ lambda {$theme_name} {ttk::style theme use $theme_name} ]
-	.main_menu.themes add command -label $theme_name -command {puts $theme_name}
-	# .main_menu.themes add command -label $theme_name -command [ lambda $theme_name {exx $theme_name} ]
+	.main_menu.themes add command -label $theme_name -command "ttk::style theme use $theme_name"
 }
 
 
