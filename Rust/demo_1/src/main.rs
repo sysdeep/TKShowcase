07e5743dcfd8c7f573acd76c4d784d6360322a8b
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2025-12-27 14:34:30 +0300

    try rust

--- Rust/demo_1/src/main.rs
+++ Rust/demo_1/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
+use tk::cmd::*;
+use tk::*;
+
+mod menu;
+
+fn main() -> TkResult<()> {
     println!("Hello, world!");
+
+    let tk = make_tk!()?;
+    let root = tk.root();
+
+    // menu::make_menu(root);
+
+    let main_frame = root.add_frame(())?.pack(-fill("both") - expand(true))?;
+
+    main_frame
+        .add_button(-text("Exit") - command("destroy ."))?
+        .pack(-side("bottom"))?;
+
+    let nb = main_frame
+        .add_ttk_notebook(())?
+        .pack(-side("top") - expand(true) - fill("both") - padx(10) - pady(10))?;
+    let p1 = nb.add_ttk_frame(())?;
+    let p2 = nb.add_ttk_frame(())?;
+    let p3 = nb.add_ttk_frame(())?;
+    let p4 = nb.add_ttk_frame(())?;
+    nb.add(&p1, -text("Widgets"))?;
+    nb.add(&p2, -text("Panels"))?;
+    nb.add(&p3, -text("Canvas"))?;
+    nb.add(&p4, -text("Dialogs"))?;
+
+    // bind Ctrl+q
+    root.bind(event::control().key_press(TkKey::q), "destroy .")?;
+
+    Ok(main_loop())
 }
---
+++ Rust/demo_1/src/menu.rs
@@ -0,0 +1,11 @@
+// use tk::cmd::*;
+// use tk::*;
+
+// pub fn make_menu<Inst: TkInstance>(root: TkRoot<Inst>) {
+//     let main_menu = root.add_menu(());
+//     root.configure(-menu(main_menu));
+
+//     let win = root.add_toplevel(())?;
+//     let menubar = win.add_menu(())?;
+//     win.configure(-menu(menubar))?;
+// }
